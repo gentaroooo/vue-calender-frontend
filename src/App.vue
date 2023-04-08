00228dbb4e04c140ed305668e1a9a9f9f0d8a7b0
gentaro <going3956@yahoo.co.jp>
2023-04-08 12:58:21 +0900

    prettier

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,9 @@
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
-  name: "App",
   components: {
     HelloWorld,
   },
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,11 +2,12 @@
   <div>
     <p>{{ message }}</p>
   </div>
+  <p>Hello World!</p>
+  <p>Hello World! Hello World! Hello World! Hello World! Hello World! Hello World!</p>
 </template>
 
 <script>
-  export default {
-    name: "HelloWorld",
-    props: ["message"],
-  };
-  </script>
+export default {
+  name: 'HelloWorld',
+};
+</script>
