76375c862d74edf88923394ccd81f903d1e67206
gentaro <going3956@yahoo.co.jp>
2023-06-25 23:41:23 +0900

    予定を表示

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <h1 class="text-h1">Calendar</h1>
-    <v-list>
-      <v-list-item v-for="event in events" :key="event.id">
-        {{ event.name }}
-      </v-list-item>
-    </v-list>
-    <v-btn type="submit" @click="fetchEvents()">fetchEvents</v-btn>
+    <v-sheet height="100vh">
+      <v-calendar
+      v-model="value"
+      :events="events"
+      @change="fetchEvents"
+      ></v-calendar>
+    </v-sheet>
   </div>
 </template>
 
@@ -15,6 +15,9 @@ import { mapGetters, mapActions } from 'vuex';
 
 export default {
   name: 'Calendar',
+  data: () => ({
+      value: new Date('2021/07/01'),  // 表示する月を指定
+    }),
   computed: {
     ...mapGetters('events', ['events']),
   },
