2ce481fba1de4a5b0f0d5ae79e1e3e447fe53280
gentaro <going3956@yahoo.co.jp>
2023-06-26 23:20:50 +0900

    月を表示

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,25 +1,38 @@
 <template>
   <div>
-    <v-sheet height="100vh">
+    <v-sheet height="6vh" class="d-flex align-center">
+      <v-btn icon>
+        <v-icon>mdi-chevron-left</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-chevron-right</v-icon>
+      </v-btn>
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
+    </v-sheet>
+    <v-sheet height="94vh">
       <v-calendar
-      v-model="value"
-      :events="events"
-      @change="fetchEvents"
+        v-model="value"
+        :events="events"
+        @change="fetchEvents"
       ></v-calendar>
     </v-sheet>
   </div>
 </template>
 
 <script>
+import { format } from 'date-fns';
 import { mapGetters, mapActions } from 'vuex';
 
 export default {
   name: 'Calendar',
   data: () => ({
-      value: new Date('2021/07/01'),  // 表示する月を指定
-    }),
+    value: new Date('2020-10-01'), // 表示する月を指定
+  }),
   computed: {
     ...mapGetters('events', ['events']),
+    title() {
+      return format(this.value, 'yyyy年 M月');
+    },
   },
   methods: {
     ...mapActions('events', ['fetchEvents']),
