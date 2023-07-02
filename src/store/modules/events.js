684d875e85f265a32f77f12c4e3161baf9cfa7f1
gentaro <going3956@yahoo.co.jp>
2023-07-02 10:48:49 +0900

    予定の出し分け

--- src/store/modules/events.js
+++ src/store/modules/events.js
@@ -5,36 +5,45 @@ const apiUrl = 'http://localhost:3000';
 const state = {
   events: [],
   event: null,
+  isEditMode: false,
 };
 
 const getters = {
-  events: state => state.events.map(event => {
-    return {
-      ...event,
-      start: new Date(event.start),
-      end: new Date(event.end)
-    };
-  }),
-  event: state => state.event ? {
-    ...state.event,
-    start: new Date(state.event.start),
-    end: new Date(state.event.end)
-  } : null,
+  events: state =>
+    state.events.map(event => {
+      return {
+        ...event,
+        start: new Date(event.start),
+        end: new Date(event.end)
+      };
+    }),
+  event: state =>
+    state.event
+      ? {
+          ...state.event,
+          start: new Date(state.event.start),
+          end: new Date(state.event.end)
+        }
+      : null,
+  isEditMode: state => state.isEditMode,
 };
-
 
 const mutations = {
   setEvents: (state, events) => (state.events = events),
   setEvent: (state, event) => (state.event = event),
+  setEditMode: (state, bool) => (state.isEditMode = bool),
 };
 
 const actions = {
   async fetchEvents({ commit }) {
     const response = await axios.get(`${apiUrl}/events`);
-    commit('setEvents', response.data); // mutationを呼び出す
+    commit('setEvents', response.data);
   },
   setEvent({ commit }, event) {
     commit('setEvent', event);
+  },
+  setEditMode({ commit }, bool) {
+    commit('setEditMode', bool)
   },
 };
 
