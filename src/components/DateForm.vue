0e4a4099c0dc537ad9c9419e454d07f28cdac1d4
gentaro <going3956@yahoo.co.jp>
2023-07-04 23:14:36 +0900

    カレンダー表示

---
+++ src/components/DateForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-date-picker
+      :value="value.replace(/\//g, '-')"
+      @input="$emit('input', $event.replace(/-/g, '/'))"
+      no-title
+      locale="ja-ja"
+      :day-format="value => new Date(value).getDate()"
+    ></v-date-picker>
+    <p>{{ value }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DateForm',
+  props: ['value'],
+}
+</script>
