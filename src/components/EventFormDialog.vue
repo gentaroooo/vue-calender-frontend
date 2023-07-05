05ab50b6f2a7fdf8251bb42e227a81afe6a1263d
gentaro <going3956@yahoo.co.jp>
2023-07-06 07:52:11 +0900

    時間選択

--- src/components/EventFormDialog.vue
+++ src/components/EventFormDialog.vue
@@ -9,11 +9,11 @@
       <DialogSection icon="mdi-square" :color="event.color">
         <v-text-field v-model="name" label="タイトル"></v-text-field>
       </DialogSection>
-    </v-card-text>
-    <v-card-text>
       <DialogSection icon="mdi-clock-outline">
         <DateForm v-model="startDate" />
+        <TimeForm v-model="startTime" />
         <DateForm v-model="endDate" />
+        <TimeForm v-model="endTime" />
       </DialogSection>
     </v-card-text>
     <v-card-actions class="d-flex justify-end">
@@ -26,24 +26,30 @@
 import { mapGetters, mapActions } from 'vuex';
 import DialogSection from './DialogSection';
 import DateForm from './DateForm';
+import TimeForm from './TimeForm';
 
 export default {
   name: 'EventFormDialog',
   components: {
     DialogSection,
     DateForm,
+    TimeForm,
   },
   data: () => ({
     name: '',
     startDate: null,
+    startTime: null,
     endDate: null,
+    endTime: null,
   }),
   computed: {
     ...mapGetters('events', ['event']),
   },
   created() {
     this.startDate = this.event.startDate;
+    this.startTime = this.event.startTime;
     this.endDate = this.event.endDate;
+    this.endTime = this.event.endTime;
   },
   methods: {
     ...mapActions('events', ['setEvent', 'setEditMode', 'createEvent']),
@@ -54,8 +60,8 @@ export default {
     submit() {
       const params = {
         name: this.name,
-        start: this.startDate,
-        end: this.endDate,
+        start: `${this.startDate} ${this.startTime || ''}`,
+        end: `${this.endDate} ${this.endTime || ''}`,
       };
       this.createEvent(params);
       this.closeDialog();
